80a5bb5b212314b7978147779f9f6a71251d870a
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-06 23:44:55 +0530

    Time: 59 ms (14.94%), Space: 144 MB (9.57%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,34 +1,35 @@
 class Solution {
-    int count=0;boolean flag=true;
+    int count=0;
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        boolean check[][]=new boolean[grid1.length][grid1[0].length];
+        boolean check[][]=new boolean[grid1.length][grid1[0].length];boolean flag=true;
         for(int i=0;i<grid1.length;i++){
             for(int j=0;j<grid1[0].length;j++){
                 if(grid2[i][j]==1&&check[i][j]==false){
                     // System.out.print(" i="+i+" j="+j);
                     flag=true;
                     
-                    dfs(grid1,grid2,check,i,j);
-                    if(flag)count++;
+                    // dfs(grid1,grid2,check,i,j);
+                    if(dfs(grid1,grid2,check,i,j,flag))count++;
                 }
             }
         }
         // System.out.print(Arrays.deepToString(check));
         return count;
     }
-    public void dfs(int[][] grid1,int[][] grid2,boolean[][]check,int i , int j){
+    public boolean dfs(int[][] grid1,int[][] grid2,boolean[][]check,int i , int j,boolean flag){
         // System.out.print(" i="+i+" j="+j+" flag="+flag+"||");
         if(i<0||i>grid1.length-1||j<0||j>grid1[0].length-1||grid2[i][j]==0||check[i][j]==true){
-            return;
+            return true;
         }
         if(grid2[i][j]==1&&grid1[i][j]!=1){flag=false;check[i][j]=true;}
         // if(grid1[i][j]==1&&grid2 [i][j]==0){check[i][j]=true;count--;return;}
 
         check[i][j]=true;
-        dfs(grid1,grid2,check,i+1,j);
-        dfs(grid1,grid2,check,i,j+1);
-        dfs(grid1,grid2,check,i-1,j);
-        dfs(grid1,grid2,check,i,j-1);
-
+        boolean a=dfs(grid1,grid2,check,i+1,j,flag);
+        boolean b=dfs(grid1,grid2,check,i,j+1,flag);
+        boolean c=dfs(grid1,grid2,check,i-1,j,flag);
+        boolean d=dfs(grid1,grid2,check,i,j-1,flag);
+        if(a&&b&&c&&d&&flag)return true;
+        return false;
     }
 }
