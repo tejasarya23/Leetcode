359d8c40d0db8d65aed655f76b3057bfb1ee70b1
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-08 11:26:10 +0530

    Time: 4 ms (47.04%), Space: 49.4 MB (56.83%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,21 +1,55 @@
 class Solution {
     public int jump(int[] nums) {
-        if(nums.length==1)return 0;
-        int dp[]= new int[nums.length];
-        int cur=0;
-        for(int i=0;i<=Math.min(nums.length-1,nums[0]);i++){
-            dp[i]=1;
-        }
-        // System.out.print(Arrays.toString(dp));
-        for(int i=1;i<nums.length;i++){
-            cur=dp[i];
-            for(int j=i;j<=Math.min(nums.length-1,i+nums[i]);j++){
-                if(dp[j]==0){
-                    dp[j]=cur+1;
-                }
+        
+        if(nums.length == 1)
+            return 0;
+        
+        if(nums[0] == 0)
+            return -1;
+        
+        int maxReach = nums[0];
+        int steps = nums[0];
+        int jumps = 1;
+        
+        for(int i = 1 ; i< nums.length ; i++){
+            
+            if(i == nums.length - 1)
+                return jumps;
+            
+            maxReach = Math.max(maxReach , i + nums[i]);
+            steps--;
+            if(steps == 0){
+                jumps++;
+                if(i >= maxReach)
+                    return -1;
+                
+                steps = maxReach - i;
             }
+            
         }
-        // System.out.print(Arrays.toString(dp));
-        return dp[nums.length-1];
+        
+        return jumps;
     }
+    
 }
+// class Solution {
+//     public int jump(int[] nums) {
+//         if(nums.length==1)return 0;
+//         int dp[]= new int[nums.length];
+//         int cur=0;
+//         for(int i=0;i<=Math.min(nums.length-1,nums[0]);i++){
+//             dp[i]=1;
+//         }
+//         // System.out.print(Arrays.toString(dp));
+//         for(int i=1;i<nums.length;i++){
+//             cur=dp[i];
+//             for(int j=i;j<=Math.min(nums.length-1,i+nums[i]);j++){
+//                 if(dp[j]==0){
+//                     dp[j]=cur+1;
+//                 }
+//             }
+//         }
+//         // System.out.print(Arrays.toString(dp));
+//         return dp[nums.length-1];
+//     }
+// }
