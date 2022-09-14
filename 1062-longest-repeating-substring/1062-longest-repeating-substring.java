f335cf150f9872a661793c2ac33ec66c1b4a8786
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-14 15:07:00 +0530

    Time: 17 ms (48.44%), Space: 48.2 MB (52.60%) - LeetHub

---
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.java
@@ -0,0 +1,31 @@
+class Solution {
+  /*
+    Search a substring of given length
+    that occurs at least 2 times.
+    Return start position if the substring exits and -1 otherwise.
+    */
+  public int search(int L, int n, String S) {
+    HashSet<String> seen = new HashSet();
+    String tmp;
+    for(int start = 0; start < n - L + 1; ++start) {
+      tmp = S.substring(start, start + L);
+      if (seen.contains(tmp)) return start;
+      seen.add(tmp);
+    }
+    return -1;
+  }
+
+  public int longestRepeatingSubstring(String S) {
+    int n = S.length();
+    // binary search, L = repeating string length
+    int left = 1, right = n;
+    int L;
+    while (left <= right) {
+      L = left + (right - left) / 2;
+      if (search(L, n, S) != -1) left = L + 1;
+      else right = L - 1;
+    }
+
+    return left - 1;
+  }
+}
