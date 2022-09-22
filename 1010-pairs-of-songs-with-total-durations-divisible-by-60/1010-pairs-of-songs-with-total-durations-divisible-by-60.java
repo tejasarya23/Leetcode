3369fd7ae7cce5841654540aaa3ab8e22974feb0
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-22 23:54:06 +0530

    Time: 7 ms (54.62%), Space: 53.6 MB (77.56%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -1,23 +1,18 @@
 class Solution {
     public int numPairsDivisibleBy60(int[] time) {
-        Map<Integer,Integer> mp = new HashMap<>();
-        long count=0;
-        for(int i=0;i<time.length;i++){
-            mp.put(time[i]%60,mp.getOrDefault(time[i]%60,0)+1);
-        }
-        // System.out.print(mp);
-        for(var key : mp.keySet()){
-            if(key<30&&mp.containsKey(60-key)){
-                count+=mp.get(key)*mp.get(60-key);
-            }else if(key==30||key==0){
-                int num=mp.get(key);
-                if(num%2==0){
-                    count+=(num/2)*(num-1);
-                }else{
-                    count+=(num)*(num-1)/2;
-                }
+        int[] remainders = new int[60];
+        int count = 0;
+        for (int t : time) {
+            if(t % 60 == 0) {
+                count += remainders[0];
+            } else {
+                count += remainders[60 - t % 60]; // how many previous nums can i match with
             }
+            
+            remainders[t % 60]++; // add for future nums to use
         }
-        return (int)count;
+        
+        return count;
+        
     }
 }
