bb440bdca6ab2c55129741e7793f360580803629
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-24 14:15:14 +0530

    Time: 1 ms (100.00%), Space: 51.7 MB (11.72%) - LeetHub

--- 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.java
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.java
@@ -1,19 +1,19 @@
 class Solution {   
     public int longestConsecutive(TreeNode root) {
-        return helper(root,root.val+1,0,0)+1;
+        return helper(root,root.val+1,0);
     }
-    public int helper(TreeNode root,int prevval,int cur,int max){
+    public int helper(TreeNode root,int prevval,int cur){
         if(root==null){
-            return max ;
+            return cur ;
         }
         if(root.val==prevval){
             cur++;
         }else{
-            cur=0;
+            cur=1;
         }
-        max=Math.max(cur,max);
-        int l=helper(root.left,root.val+1,cur,max);
-        int r=helper(root.right,root.val+1,cur,max);
-        return Math.max(l,r);
+        // max=Math.max(cur,max);
+        int l=helper(root.left,root.val+1,cur);
+        int r=helper(root.right,root.val+1,cur);
+        return Math.max(l,Math.max(r,cur));
     }
 }
