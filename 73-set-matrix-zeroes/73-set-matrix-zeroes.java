8ec3a6d12041fd70bfa93cef81e81ae11482bf93
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-26 13:08:27 +0530

    Time: 2 ms (52.29%), Space: 54.3 MB (37.37%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -3,14 +3,14 @@ class Solution {
         boolean col0=false;
         for(int i=0;i<matrix.length;i++){
             if(matrix[i][0]==0){
-                col0=true;break;
+                col0=true;
             }
         }
         // System.out.print(col0);
         boolean row0=false;
         for(int i=0;i<matrix[0].length;i++){
             if(matrix[0][i]==0){
-                row0=true;break;
+                row0=true;
             }
         }
         // System.out.print(row0);
