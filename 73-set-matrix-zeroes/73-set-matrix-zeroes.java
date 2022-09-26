446f6e46b2c6552a67e95e1293b7e73e9716039b
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-26 13:06:59 +0530

    Time: 8 ms (6.17%), Space: 43.3 MB (99.89%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,51 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        boolean col0=false;
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                col0=true;
+            }
+        }
+        System.out.print(col0);
+        boolean row0=false;
+        for(int i=0;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                row0=true;
+            }
+        }
+        System.out.print(row0);
+        for(int i=1;i<matrix.length;i++){
+            for( int j=1;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                }
+            }
+        }       
+        for(int i=1;i<matrix.length;i++){
+            if(matrix[i][0]==0){
+                for(int j=1;j<matrix[0].length;j++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        System.out.print(Arrays.deepToString(matrix));   
+        for(int i=1;i<matrix[0].length;i++){
+            if(matrix[0][i]==0){
+                for(int j=1;j<matrix.length;j++){
+                    matrix[j][i]=0;
+                }
+            }
+        }
+        if(col0){
+            for(int i=0;i<matrix.length;i++){
+                matrix[i][0]=0;
+            }
+        }
+        if(row0){
+            for(int i=0;i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
+        }
+    }
+}
