2fc85a0cf014a84edecb91d2dcfdb497d1f3bc16
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-26 13:44:42 +0530

    Time: 1 ms (100.00%), Space: 41.6 MB (81.65%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,17 +1,38 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer,Integer> mp = new HashMap<>();
-        for(int num : arr){
-            mp.put(num,mp.getOrDefault(num,0)+1);
+        int[] count = new int[2001];
+        for (int num : arr) {
+            count[num + 1000]++;
         }
-        Set<Integer> st = new HashSet<>();
-        for(var freq: mp.values()){
-            if(st.contains(freq)){
+        for (int i = 0; i < count.length; i++) {
+            int num = count[i];
+            while (num > 0 && num != count[num]) {
+                count[i] = count[num];
+                count[num] = num;
+                num = count[i];
+            }
+            if (num != 0) {
                 return false;
-            }else{
-                st.add(freq);
             }
         }
         return true;
     }
 }
+
+// class Solution {
+//     public boolean uniqueOccurrences(int[] arr) {
+//         Map<Integer,Integer> mp = new HashMap<>();
+//         for(int num : arr){
+//             mp.put(num,mp.getOrDefault(num,0)+1);
+//         }
+//         Set<Integer> st = new HashSet<>();
+//         for(var freq: mp.values()){
+//             if(st.contains(freq)){
+//                 return false;
+//             }else{
+//                 st.add(freq);
+//             }
+//         }
+//         return true;
+//     }
+// }
