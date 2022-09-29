08b4f5f844bdf05bb1ea225d720f1affb9564545
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-30 02:07:26 +0530

    Time: 12 ms (58.66%), Space: 54.7 MB (40.53%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,26 +1,67 @@
 class Solution {
-    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
     public int maximalNetworkRank(int n, int[][] roads) {
-        int[] deg = new int[n];
-        for(int i = 0 ; i < n ; i++)
-            adj.add(new ArrayList<Integer>());
-        for(int[] i : roads){
-            int u = i[0];
-            int v = i[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-            deg[u]++;
-            deg[v]++;
+        // calculate all node degrees
+        int[] degrees = new int[n];
+        for (int[] road : roads) {
+            degrees[road[0]]++;
+            degrees[road[1]]++;
         }
-        int ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = i + 1 ; j < n ; j++){
-                int temp = deg[i] + deg[j];
-                if(adj.get(i).contains(j))
-                    temp--;
-                ans = Math.max(ans , temp);
+        
+        // find first max and second max degree
+        int firstMax = 0, secondMax = 0;
+        for (int degree : degrees) {
+            if (degree > firstMax) {
+                secondMax = firstMax;
+                firstMax =  degree;
+            }
+            else if (degree > secondMax && degree < firstMax) {
+                secondMax = degree;
             }
         }
-        return ans;
+        
+        // there can be multiple node with firstMax degree or secondMax degree
+        // find the count of such nodes
+        int fmCount = 0, smCount = 0;
+        for (int degree : degrees) {
+            if (degree == firstMax) fmCount++;
+            if (degree == secondMax) smCount++;
+        }
+        
+        // if there are more than one node with degree = 'firstMax'
+        // then we can consider 'firstMax' twice and we don't need to use 'secondMax'
+        if (fmCount > 1) {
+            // here the answer in general will be 2*firstMax
+            // but if the two nodes that we're considering are connected, then we'll have to subtract 1
+            // in other words, if there are atleast one such pair, whose degree = 'firstMax' and
+            // they're not connected then answer is 2*firstMax, else if all nodes with degree = 'firstMax'
+            // are connected to each other, then answer is 2*firstMax-1, as we'll have one common edge
+            int edgeCount = 0;
+            for (int[] road : roads)
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == firstMax) edgeCount++;
+            
+            // maximum number of edges possible with all the nodes with degree = 'firstMax'
+            int maxEdgeWithFirstMax = fmCount * (fmCount-1) / 2;
+            
+            // if all nodes with degree = 'firstMax' are connected to each other, then subtract 1
+            return 2 * firstMax - (maxEdgeWithFirstMax == edgeCount ? 1 : 0);
+        }
+        
+        // if there is only one node with degree = 'firstMax'
+        // then we'll need to use node with degree = 'secondMax'
+        else {
+            // here the answer in general will be (firstMax + secondMax)
+            // but if the two nodes that we're considering are connected, then we'll have to subtract 1
+            // in other words, if there are atleast one such pair, whose degree = 'firstMax' and 'secondMax'
+            // and they're not connected then answer is (firstMax + secondMax), else if all pairs
+            // are connected to each other, then answer is (firstMax + secondMax - 1), as we'll have one common edge
+            int edgeCount = 0;
+            for (int[] road : roads) {
+                if (degrees[road[0]] == firstMax && degrees[road[1]] == secondMax) edgeCount++;
+                if (degrees[road[0]] == secondMax && degrees[road[1]] == firstMax) edgeCount++;
+            }
+            
+            // if all pairs are connected to each other, then subtract 1
+            return firstMax + secondMax - (smCount == edgeCount ? 1 : 0);
+        }
     }
 }
