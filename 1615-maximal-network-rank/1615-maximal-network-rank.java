7647e69d569475c0c6ab82f45054ef29f9d1fd91
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-09-30 01:42:02 +0530

    Time: 66 ms (17.07%), Space: 53.9 MB (70.06%) - LeetHub

---
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -0,0 +1,26 @@
+class Solution {
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    public int maximalNetworkRank(int n, int[][] roads) {
+        int[] deg = new int[n];
+        for(int i = 0 ; i < n ; i++)
+            adj.add(new ArrayList<Integer>());
+        for(int[] i : roads){
+            int u = i[0];
+            int v = i[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+            deg[u]++;
+            deg[v]++;
+        }
+        int ans = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                int temp = deg[i] + deg[j];
+                if(adj.get(i).contains(j))
+                    temp--;
+                ans = Math.max(ans , temp);
+            }
+        }
+        return ans;
+    }
+}
