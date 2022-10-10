7d3dc4a2de94791d8b026a27b4308263c5f34078
tejasarya23 <92249834+tejasarya23@users.noreply.github.com>
2022-10-10 13:18:06 +0530

    Time: 117 ms (41.25%), Space: 66.2 MB (76.95%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int sumSubarrayMins(int[] A) {
-        Stack<Integer> stack = new Stack<>();
-        int[] dp = new int[A.length + 1];
-        stack.push(-1);
-        int result = 0, M = (int)1e9 + 7;
+        public int sumSubarrayMins(int[] A) {
+        Stack<Integer> s = new Stack<>();
+        int[] dp = new int[A.length];
+        int total = 0;
         for (int i = 0; i < A.length; i++) {
-            while (stack.peek() != -1 && A[i] <= A[stack.peek()]) {
-                stack.pop();
-            }
-            dp[i + 1] = (dp[stack.peek() + 1] + (i - stack.peek()) * A[i]) % M;
-            stack.push(i);
-            result += dp[i + 1];
-            result %= M;
+            while (!s.isEmpty() && A[s.peek()] > A[i]) 
+                s.pop();
+            if (s.isEmpty()) 
+                dp[i] = (i + 1) * A[i];
+            else 
+                dp[i] = dp[s.peek()] + (i - s.peek()) * A[i];
+            total = (total + dp[i] ) % 1000000007;
+            s.push(i);
         }
-        return result;
+        return total;
     }
 }
